ec6b549e94df727bf6a39f53f7cdb75f01e6db82
Rodry20 <rodrybarretofernandez@gmail.com>
2024-09-18 17:00:23 -0500

    fix model clases in login form

--- ToDoListT2/LoginForm.cs
+++ ToDoListT2/LoginForm.cs
@@ -6,23 +6,6 @@ using Models;
 
 namespace AppForms
 {
-    public class LoginBodyRequest
-    {
-        public string email { get; set; }
-        public string password { get; set; }
-    }
-
-    public class ResponseUser
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string email { get; set; }
-    }
-    public class LoginBodyResponse
-    {
-        public ResponseUser user { get; set; }
-        public string token { get; set; }
-    }
     public partial class LoginForm : Form
     {
         public LoginForm()
---
+++ ToDoListT2/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿namespace Models
+{
+    public class LoginBodyRequest
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+
+    public class ResponseUser
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+    public class LoginBodyResponse
+    {
+        public ResponseUser user { get; set; }
+        public string token { get; set; }
+    }
+}
