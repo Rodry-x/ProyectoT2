31525cc143a0b64fd8dd7bec4daacca6237610d1
AriaGT <ariagt191000@gmail.com>
2024-10-01 07:07:28 -0500

    fix home form events names

--- ToDoListT2/Forms/HomeForm.Designer.cs
+++ ToDoListT2/Forms/HomeForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.msg_welcome = new System.Windows.Forms.Label();
             this.subTitelList = new System.Windows.Forms.Label();
-            this.listBoxView = new System.Windows.Forms.ListBox();
+            this.tasksList = new System.Windows.Forms.ListBox();
             this.logout_btn = new System.Windows.Forms.Button();
-            this.button_add_task = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // msg_welcome
@@ -47,9 +47,9 @@
             this.msg_welcome.Size = new System.Drawing.Size(276, 46);
             this.msg_welcome.TabIndex = 0;
             this.msg_welcome.Text = "msg_welcome";
-            //
+            // 
             // subTitelList
-            //
+            // 
             this.subTitelList.AutoSize = true;
             this.subTitelList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.subTitelList.ForeColor = System.Drawing.SystemColors.Control;
@@ -59,19 +59,19 @@
             this.subTitelList.Size = new System.Drawing.Size(157, 20);
             this.subTitelList.TabIndex = 2;
             this.subTitelList.Text = "Quieres anotar algo?";
-            //
-            // listBoxView
-            //
-            this.listBoxView.FormattingEnabled = true;
-            this.listBoxView.Location = new System.Drawing.Point(15, 178);
-            this.listBoxView.Margin = new System.Windows.Forms.Padding(2, 13, 2, 13);
-            this.listBoxView.Name = "listBoxView";
-            this.listBoxView.Size = new System.Drawing.Size(406, 472);
-            this.listBoxView.TabIndex = 3;
-            this.listBoxView.DoubleClick += new System.EventHandler(this.OnListDoubleClick);
-            //
+            // 
+            // tasksList
+            // 
+            this.tasksList.FormattingEnabled = true;
+            this.tasksList.Location = new System.Drawing.Point(15, 178);
+            this.tasksList.Margin = new System.Windows.Forms.Padding(2, 13, 2, 13);
+            this.tasksList.Name = "tasksList";
+            this.tasksList.Size = new System.Drawing.Size(406, 472);
+            this.tasksList.TabIndex = 3;
+            this.tasksList.DoubleClick += new System.EventHandler(this.List_DoubleClick);
+            // 
             // logout_btn
-            //
+            // 
             this.logout_btn.Location = new System.Drawing.Point(15, 22);
             this.logout_btn.Margin = new System.Windows.Forms.Padding(2);
             this.logout_btn.Name = "logout_btn";
@@ -79,28 +79,28 @@
             this.logout_btn.TabIndex = 4;
             this.logout_btn.Text = "Cerrar Sesión";
             this.logout_btn.UseVisualStyleBackColor = true;
-            this.logout_btn.Click += new System.EventHandler(this.logout_btn_Click);
-            //
-            // button_add_task
-            //
-            this.button_add_task.Location = new System.Drawing.Point(16, 148);
-            this.button_add_task.Margin = new System.Windows.Forms.Padding(2);
-            this.button_add_task.Name = "button_add_task";
-            this.button_add_task.Size = new System.Drawing.Size(90, 22);
-            this.button_add_task.TabIndex = 10;
-            this.button_add_task.Text = "Añadir";
-            this.button_add_task.UseVisualStyleBackColor = true;
-            this.button_add_task.Click += new System.EventHandler(this.button_add_task_Click);
-            //
+            this.logout_btn.Click += new System.EventHandler(this.ButtonLogout_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(331, 153);
+            this.buttonAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(90, 22);
+            this.buttonAdd.TabIndex = 10;
+            this.buttonAdd.Text = "Añadir";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
             // HomeForm
-            //
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkCyan;
             this.ClientSize = new System.Drawing.Size(434, 670);
-            this.Controls.Add(this.button_add_task);
+            this.Controls.Add(this.buttonAdd);
             this.Controls.Add(this.logout_btn);
-            this.Controls.Add(this.listBoxView);
+            this.Controls.Add(this.tasksList);
             this.Controls.Add(this.subTitelList);
             this.Controls.Add(this.msg_welcome);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -115,9 +115,9 @@
 
         private System.Windows.Forms.Label msg_welcome;
         private System.Windows.Forms.Label subTitelList;
-        private System.Windows.Forms.ListBox listBoxView;
+        private System.Windows.Forms.ListBox tasksList;
         private System.Windows.Forms.Button logout_btn;
-        private System.Windows.Forms.Button button_add_task;
+        private System.Windows.Forms.Button buttonAdd;
     }
 }
 
--- ToDoListT2/Forms/HomeForm.cs
+++ ToDoListT2/Forms/HomeForm.cs
@@ -13,13 +13,13 @@ namespace Forms
         {
             InitializeComponent();
         }
+
         private async void HomeForm_Load(object sender, EventArgs e)
         {
             msg_welcome.Text = $"Bienvenido, {DataStore.User.Name}";
-
-            listBoxView.DataSource = DataStore.Tasks;
-            listBoxView.DisplayMember = "Name";
-            listBoxView.ValueMember = "Id";
+            tasksList.DataSource = DataStore.Tasks;
+            tasksList.DisplayMember = "Name";
+            tasksList.ValueMember = "Id";
             try
             {
                 var response = await FetchHelper.GetAsync<List<GetTasksItem>>("tasks");
@@ -39,22 +39,25 @@ namespace Forms
                 Console.WriteLine($"Error: {ex.Message}");
             }
         }
-        private void logout_btn_Click(object sender, EventArgs e)
-        {
-            DataStore.Clear();
-            FetchHelper.ClearAuthenticationHeader();
-            NavigationHelper.NavigateTo(new LoginForm());
-        }
-        private void button_add_task_Click(object sender, EventArgs e)
+
+        private void List_DoubleClick(object sender, EventArgs e)
         {
             AddTaskForm addTask = new AddTaskForm();
             addTask.Visible = true;
         }
-        private void OnListDoubleClick(object sender, EventArgs e)
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
         {
-            TaskItem selectedTask = (TaskItem)listBoxView.SelectedItem;
+            TaskItem selectedTask = (TaskItem)tasksList.SelectedItem;
             EditTaskForm editTask = new EditTaskForm(selectedTask);
             editTask.Visible = true;
+        }
+
+        private void ButtonLogout_Click(object sender, EventArgs e)
+        {
+            DataStore.Clear();
+            FetchHelper.ClearAuthenticationHeader();
+            NavigationHelper.NavigateTo(new LoginForm());
         }
     }
 }
